b12f6a4230d1138f0b87b0721ba951bf096bc4b4
ZhanXueyou <zhanxueyou@msn.com>
2016-01-12 15:50:44 +0800

    new test files

--- AssertExcept.py
+++ AssertExcept.py
@@ -3,4 +3,3 @@ try:
 	assert 1 == 0, 'One does not equal zero silly!'
 except AssertionError as args:
 	print('%s: %s' % (args.__class__.__name__, args))
-	
---
+++ robot/var.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
+
+__all__ = ['testURL', 'searchText', 'LIST__vars', 'integer']	# 所有可用的变量
+
+testURL = 'http://www.baidu.com'
+searchText = 'python'
+LIST__vars = ['hello', 'list', 'world']		#list型的变量
+integer = random.randint(1, 10)		# 返回一个随机数
+
+
+
