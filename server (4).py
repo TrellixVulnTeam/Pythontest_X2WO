a5df89c917a6c296c7ff0e2903c7e17dcd43d8a2
ZhanXueyou <zhanxueyou@msn.com>
2016-05-23 07:57:18 +0800

    merge documents

---
+++ server (4).py
@@ -0,0 +1,12 @@
+# -*- cdoing: utf-8 -*-
+
+
+from wsgiref.simple_server import make_server
+from hello import application
+
+httpd = make_server('', 8001, application)
+
+print('Serving HTTP on port 8001...')
+
+httpd.serve_forever()
+
