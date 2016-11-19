ada11d9c2dac0548a5fd87dc2e64351b33f0bbab
ZhanXueyou <zhanxueyou@126.com>
2016-11-19 17:05:51 +0800

    update

---
+++ call_mymodule.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Zhanxueyou
+""" 这里是文档
+
+"""
+import myModule
+
+myModule.func()
+
+myClass = myModule.MyClass()
+myClass.myFunc()
+
+print(__doc__)
---
+++ myModule.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Zhanxueyou
+
+def func():
+	print('my func')
+
+class MyClass():
+	def myFunc(self):
+		print("my method")
+
+if __name__ == '__main__':
+	print('main')
+else:
+	print('other')
