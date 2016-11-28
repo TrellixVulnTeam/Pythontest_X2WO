f42f369663f56b56f563cb9bffe94ba5b1f0150e
zhanxueyou <zhanxueyou@msn.com>
2016-11-28 15:55:48 +0800

    write file

--- 4.py
+++ 4.py
@@ -4,10 +4,11 @@
 #编写Hello World脚本，在系统环境中执行
 ####################
 import os
+import time
 print('Hello World!')
 
 
 print(os.path.realpath(__file__))
 with open('4.txt', 'a') as f:
-    f.write(os.path.realpath(__file__)+'\n')
+    f.write(time.strftime('%Y-%m-%d %H:%M:%S ') + os.path.realpath(__file__) + '\n')
     f.writelines('Hello'+'\n')
