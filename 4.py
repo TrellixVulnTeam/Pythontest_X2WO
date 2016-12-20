6479119beb0851ff0120c5c173318dc72e831ec2
zhanxueyou <zhanxueyou@msn.com>
2016-12-20 18:55:33 +0800

    update

--- 4.py
+++ 4.py
@@ -8,8 +8,9 @@ import time
 print('Hello World!')
 
 
-print(os.path.realpath(__file__))
-print(os.path.dirname(__file__))
+print(os.path.realpath(__file__))	# 当前文件的路径
+print(os.path.dirname(__file__))	# 当前文件的相对路径的目录，显示为空
+print(os.path.abspath(os.path.dirname(__file__)))	# 当前文件的绝对路径的目录
 # with open('4.txt', 'a') as f:
 #     f.write(time.strftime('%Y-%m-%d %H:%M:%S ') + os.path.realpath(__file__) + '\n')
 #     f.writelines('Hello'+'\n')
