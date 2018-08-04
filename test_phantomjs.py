8b36f953848dcc1b80880bf8c310220febc8e274
zhanxueyou <zhanxueyou@msn.com>
2018-08-04 18:12:50 +0800

    web UI headless test

---
+++ test_phantomjs.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+from selenium import webdriver
+
+driver = driver = webdriver.PhantomJS(executable_path='/home/zhanxueyou/green/phantomjs_2_1_1/bin/phantomjs')#这里的executable_path填你phantomJS的路径
+
+driver.get('http://www.so.com')
+
+print(driver.title)
+
+driver.quit()
